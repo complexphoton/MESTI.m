c02e5d4cda97a501b0a8d44cf84cf7ecd31fe7d4
Chia Wei Hsu <68754706+chiaweihsu@users.noreply.github.com>
2022-04-12 18:38:31 -0700

    Update metalens example

---
+++ examples/2d_metalens/asp.m
@@ -0,0 +1,57 @@
+function f = asp(f0, x, kx, ind_prop, ny_tot, ny_pad_low)
+%ASP Use angular spectrum propagation (ASP) to propagate a scalar field
+%   from f0 = f(x=0,y) to f(x,y).
+%   === Input Arguments ===
+%   f0 (numeric column vector or matrix):
+%       Initial field profile at x = x0 plane 
+%       The column index is index in transverse (y) direction. If f0
+%       is a numeric matrix, then different columns of f0 correspond 
+%       to different inputs for ASP.
+%   x (numeric scalar or row vector):
+%       Position field propagating to. If x is a column vector, 
+%       different rows of x correspond to different positions
+%       for ASP to propagate.
+%   kx (ny_tot-by-1 complex column vector):
+%       Longitudinal wave number kx for all channels, including both 
+%       propagating and evanescent ones   
+%   ind_prop (N_prop-by-1 integer column vector):
+%       Indices of the N_prop propagating channels among all ny_tot channels
+%   ny_tot (numeric scalar, real):
+%       Number of grid points in the transverse (y) direction for the ASP
+%   ny_pad_low (numeric scalar, real):
+%       Number of zeros to padon one low side.
+%   === Output Arguments ===
+%   f (numeric column vector or matrix):
+%       Field profile at x0+x plane 
+%       The column index is index in transverse (y) direction. If f
+%       is a numeric matrix, then different columns of f correspond
+%       to different outputs for ASP.
+
+if ~isequal(size(kx), [ny_tot,1])
+    error('kx must be a column vector with ny_tot = %d elements.', ny_tot);
+end
+
+if size(x,2) ~= 1 && size(f0,2) ~= 1
+    error(['Either mutliple propation distances or mutliple intial fields ' ...
+        'can be supported, but cannot input mutliple propation distances ' ...
+        'and mutliple intial fields']);    
+end
+
+% Fourier transform f(x=0,y) to f(x=0,ky), as in Eq. (S43) of the SCSA paper.
+% To get a finer spacing in ky, zeros are padded below and above f0.
+% The most straightforward implementation is:
+% f0_fft = fft([zeros(ny_pad_low,size(f0,2)); f0; zeros(ny_tot-size(f0,1)-ny_pad_low,size(f0,2))]);
+% But that comes with some redundant computations on the zeros.
+% The following is equivalent but slightly more efficient:
+f0_fft = exp((-2i*pi*ny_pad_low/ny_tot)*(0:(ny_tot-1)).').*fft(f0,ny_tot,1);
+
+% Remove the evanescent components of f(x=0,ky), propagate it to f(x,ky),
+% and ifft back to f(x,y), as in Eqs. (S41-S42) of the SCSA paper.
+% The most straightforward implementation is:
+% f = ifft(exp(1i*kx(ind_prop)*x).*f0_fft(ind_prop,:));
+% This can be evaluated efficiently as following:
+f = exp((-2i*pi/ny_tot*floor(size(ind_prop,1)/2))*(0:(ny_tot-1)).').* ...
+        ifft( ...
+        circshift(exp(1i*kx(ind_prop)*x).*f0_fft(ind_prop,:), floor(size(ind_prop,1)/2)), ...
+        ny_tot,1);
+end
--- examples/2d_metalens/build_compressed_B.m
+++ examples/2d_metalens/build_compressed_B.m
@@ -1,93 +1,60 @@
-function [phiQF_L, phiQF_R] = build_phiQF(ny, N_L, N_R, w_t_over_dx_L, w_t_over_dx_R, use_Hann_Q)
-%BUILD_PHIQF build the matrix phi*Q*F which is pre-processing step for SCSA-c.
-%To learn more details, see Sec.5 in the supplementary of the SCSA paper.       
+function phiQF = build_compressed_B(ny, M, ny_window, use_Hann_window)
+%BUILD_PHIQF build the compressed matrix phi*Q*F for SCSA-c.
+%   See supplementary section 5 of the SCSA paper.
 %
 %   === Input Arguments ===
-%   ny (numeric scalar, integer):
-%       Number of pixels along the transverse direction
-%   N_L (numeric scalar, integer):
-%       Total number of channels on the left considering 
-%   N_R (numeric scalar, integer):
-%       Total number of channels on the right considering 
-%   w_t_over_dx_L (numeric scalar, real):
-%       Truncation window over grid size on the left
-%   w_t_over_dx_R (numeric scalar, real):
-%       Truncation window over grid size on the right
-%   use_Hann_Q (logical scalar):
-%       Whether to use Hann function as weight matrix Q.
+%   ny (positive integer scalar):
+%       Number of pixels along the transverse direction.
+%   M (positive integer scalar):
+%       Number of channels. Must be no larger than ny.
+%   ny_window (positive integer scalar):
+%       Number of pixels within the truncation window. Must be between 1 and ny.
+%   use_Hann_window (logical scalar):
+%       Whether to use the Hann window function as the weight Q.
+%
 %   === Output Arguments ===
-%   phiQF_L (numeric matrix):
-%       Matrix phi*Q*F on the left
-%   phiQF_R (numeric matrix):
-%       Matrix phi*Q*F on the right
+%   phiQF (ny-by-M sparse matrix):
+%       Matrix phi*Q*F truncated to ny_window pixels around the peaks.
 
-
-% Truncation window in pixel
-ny_tr_L = min([ny, 1 + round(w_t_over_dx_L)]);
-ny_tr_R = min([ny, 1 + round(w_t_over_dx_R)]);
-
-% Channel range to be considered
-k_L = round((N_L-1)/2);
-k_R = round((N_R-1)/2);
-
-% Build the truncated version of phi_L*Q_L*F_L as a sparse matrix.
-phiQF_L = spalloc(ny, N_L, ny_tr_L*N_L); % Pre-allocate the sparse matrix. 
-for a_L = -k_L:1:k_L % Loop over input channels on the left.
-    % Keep region along y direction (index m).
-    m_center = ny + ny*a_L/N_L; % Peak location; not necessarily an integer
-    m = round(m_center - ((ny_tr_L+1)/2)) + (1:ny_tr_L);  % In interval [1, 2*ny-1]
-    m(m > ny) = m(m > ny) - ny; % Wrap around in y direction.
-
-    if use_Hann_Q
-        % Analytic expression for phi_L*Q_L*F_L
-        % See Eq. (S36) and Eq. (S38) in the supplementary of the SCSA paper paper.       
-        sin_m = sin((pi/ny*N_L)*m);
-        term1 = (0.5*((-1)^a_L)/sqrt(ny*N_L))*sin_m./sin(pi*((m/ny)-(a_L/N_L)));
-        term2 = (0.25*((-1)^(a_L-1))/sqrt(ny*N_L))*sin_m./sin(pi*((m/ny)-((a_L-1)/N_L)));
-        term3 = (0.25*((-1)^(a_L+1))/sqrt(ny*N_L))*sin_m./sin(pi*((m/ny)-((a_L+1)/N_L)));
-        % Handle situations where the denominator is zero.
-        if int32(ny*a_L/N_L)*N_L == int32(ny*a_L); term1(m == (round(ny*a_L/N_L)+ny*(a_L<=0))) = 0.5*sqrt(N_L/ny); end
-        if int32(ny*(a_L-1)/N_L)*N_L == int32(ny*(a_L-1)); term2(m == (round(ny*(a_L-1)/N_L)+ny*((a_L-1)<=0))) = 0.25*sqrt(N_L/ny); end
-        if int32(ny*(a_L+1)/N_L)*N_L == int32(ny*(a_L+1)); term3(m == (round(ny*(a_L+1)/N_L)+ny*((a_L+1)<=0))) = 0.25*sqrt(N_L/ny); end
-        phiQF_L(m, a_L+k_L+1) = term1 + term2 + term3;
-    else
-        % Analytic expression for phi_L*F_L
-        % See Eq. (S36) in the supplementary of the SCSA paper paper.
-        phiQF_L(m, a_L+k_L+1) = (((-1)^a_L)/sqrt(ny*N_L))*sin((pi/ny*N_L)*m)./sin(pi*((m/ny)-(a_L/N_L)));
-        % Handle situations where the denominator is zero.        
-        if int32(m_center)*N_L == int32(ny*(N_L+a_L)) 
-            phiQF_L(round(m_center)-ny*(a_L>0), a_L+k_L+1) = sqrt(N_L/ny);
-        end
-    end
+if M > ny
+    error('M should not be larger than ny.');
 end
 
-% Build the truncated version of phi_R*Q_R*F_R as a sparse matrix.
-phiQF_R = spalloc(ny, N_R, ny_tr_R*N_R); % Pre-allocate the sparase matrix.
-for a_R = -k_R:1:k_R  % Loop over input channels on the right.
-    % Keep region along y direction (index m).
-    m_center = ny + ny*a_R/N_R;  % Peak location; not necessarily an integer
-    m = round(m_center - ((ny_tr_R+1)/2)) + (1:ny_tr_R); 
+if ny_window > ny
+    warning('ny_window = %d is larger than ny = %d; reducing to ny.');
+    ny_window = ny;
+end
+
+% Range of channel index a
+a_min = round((M-1)/2);
+
+% Build the truncated version of phi*Q*F as a sparse matrix.
+m_L_window = 1:ny_window;
+phiQF = spalloc(ny, M, ny_window*M); % Pre-allocate the sparse matrix. 
+for a = -a_min:1:a_min % Loop over input channels on the left.
+    % Find the indices m for pixels within the truncation window
+    m_center = ny + ny*a/M; % Peak location; not necessarily an integer
+    m = round(m_center - ((ny_window+1)/2)) + m_L_window; % In interval [1, 2*ny-1]
     m(m > ny) = m(m > ny) - ny; % Wrap around in y direction.
 
-    if use_Hann_Q
-        % Analytic expression for phi_R*Q_R*F_R
-        % See Eq. (S36) and Eq. (S38) in the supplementary of the SCSA paper paper.       
-        sin_m = sin((pi/ny*N_R)*m);
-        term1 = (0.5*((-1)^a_R)/sqrt(ny*N_R))*sin_m./sin(pi*((m/ny)-(a_R/N_R)));
-        term2 = (0.25*((-1)^(a_R-1))/sqrt(ny*N_R))*sin_m./sin(pi*((m/ny)-((a_R-1)/N_R)));
-        term3 = (0.25*((-1)^(a_R+1))/sqrt(ny*N_R))*sin_m./sin(pi*((m/ny)-((a_R+1)/N_R)));
+    % Build phi*Q*F within this window
+    if use_Hann_window
+        % See Eq. (S36) and Eq. (S38) in the supplementary of the SCSA paper.
+        sin_m = sin((pi/ny*M)*m);
+        term1 = (0.5*((-1)^a)/sqrt(ny*M))*sin_m./sin(pi*((m/ny)-(a/M)));
+        term2 = (0.25*((-1)^(a-1))/sqrt(ny*M))*sin_m./sin(pi*((m/ny)-((a-1)/M)));
+        term3 = (0.25*((-1)^(a+1))/sqrt(ny*M))*sin_m./sin(pi*((m/ny)-((a+1)/M)));
         % Handle situations where the denominator is zero.
-        if int32(ny*a_R/N_R)*N_R == int32(ny*a_R); term1(m == (round(ny*a_R/N_R)+ny*(a_R<=0))) = 0.5*sqrt(N_R/ny); end
-        if int32(ny*(a_R-1)/N_R)*N_R == int32(ny*(a_R-1)); term2(m == (round(ny*(a_R-1)/N_R)+ny*((a_R-1)<=0))) = 0.25*sqrt(N_R/ny); end
-        if int32(ny*(a_R+1)/N_R)*N_R == int32(ny*(a_R+1)); term3(m == (round(ny*(a_R+1)/N_R)+ny*((a_R+1)<=0))) = 0.25*sqrt(N_R/ny); end
-        phiQF_R(m, a_R+k_R+1) = term1 + term2 + term3;
+        if int32(ny*a/M)*M == int32(ny*a); term1(m == (round(ny*a/M)+ny*(a<=0))) = 0.5*sqrt(M/ny); end
+        if int32(ny*(a-1)/M)*M == int32(ny*(a-1)); term2(m == (round(ny*(a-1)/M)+ny*((a-1)<=0))) = 0.25*sqrt(M/ny); end
+        if int32(ny*(a+1)/M)*M == int32(ny*(a+1)); term3(m == (round(ny*(a+1)/M)+ny*((a+1)<=0))) = 0.25*sqrt(M/ny); end
+        phiQF(m, a+a_min+1) = term1 + term2 + term3;
     else
-        % Analytic expression for phi_R*F_R        
-        % See Eq. (S36) in the supplementary of the SCSA paper paper.
-        phiQF_R(m, a_R+k_R+1) = (((-1)^a_R)/sqrt(ny*N_R))*sin((pi/ny*N_R)*m)./sin(pi*((m/ny)-(a_R/N_R)));
-        % Handle situations where the denominator is zero.                
-        if int32(m_center)*N_R == int32(ny*(N_R+a_R))
-            phiQF_R(round(m_center)-ny*(a_R>0), a_R+k_R+1) = sqrt(N_R/ny);
+        % See Eq. (S36) in the supplementary of the SCSA paper.
+        phiQF(m, a+a_min+1) = (((-1)^a)/sqrt(ny*M))*sin((pi/ny*M)*m)./sin(pi*((m/ny)-(a/M)));
+        % Handle situations where the denominator is zero.
+        if int32(m_center)*M == int32(ny*(M+a))
+            phiQF(round(m_center)-ny*(a>0), a+a_min+1) = sqrt(M/ny);
         end
     end
 end
